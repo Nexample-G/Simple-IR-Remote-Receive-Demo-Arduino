7646caa71a093a04b6d616f7007c9ca92fd6f851
Nexample <80260293+Nexample-G@users.noreply.github.com>
2021-10-08 18:53:17 +0530

    Add files via upload

---
+++ Simple IR Remote Receive Demo Arduino.ino
@@ -0,0 +1,33 @@
+#include <IRremote.h>
+  IRrecv irrecv(A3);
+  decode_results IR;
+void setup(){
+  Serial.begin(9600);
+  irrecv.enableIRIn();
+}
+void loop() {
+  if (irrecv.decode(&IR)) {
+      if (0) {
+            Serial.println(IR.value, HEX);
+      }
+switch(IR.value){
+  case 0x1FE50AF:  BTIR1();break;
+  case 0x1FED827:  BTIR2();break;
+  case 0x1FEF807:  BTIR3();break;
+  case 0x1FE30CF:  BTIR4();break;
+default : break;}
+     irrecv.resume();
+  }
+}
+void BTIR1(){
+        Serial.println('1');
+}
+void BTIR2(){
+        Serial.println('2');
+}
+void BTIR3(){
+        Serial.println('3');
+}
+void BTIR4(){
+        Serial.println('4');
+}
